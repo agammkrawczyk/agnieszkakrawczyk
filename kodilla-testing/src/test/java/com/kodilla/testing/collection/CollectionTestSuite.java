4587dd9241fc19c1451a6ee7261f33cceaed2390
agnieszka <agammkrawczyk@gmail.com>
2018-05-28 22:12:14 +0200

    adding annotations before and after

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -7,6 +7,24 @@ import java.util.*;
 import static org.junit.Assert.*;
 
 public class CollectionTestSuite {
+    @Before
+    public void before(){
+        System.out.println("Test Case : begin");
+    }
+    @After
+    public void after(){
+        System.out.println("Test Case: end");
+    }
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Test Letters Odd  or Even : begin");
+    }
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("Test Letters Odd  or Even: end");
+    }
+
+
     //testing emptylist
     @Test
       public void testOddNumbersExterminatorEmptyList () {
@@ -25,7 +43,7 @@ public class CollectionTestSuite {
 
         //Given
         List<Integer> actual = Arrays.asList(1,2,3,4);
-        List<Integer> expected= Arrays.asList(2,4);
+        List<Integer> expected = Arrays.asList(2,4);
 
         OddNumbersExterminator tester = new OddNumbersExterminator();
 
