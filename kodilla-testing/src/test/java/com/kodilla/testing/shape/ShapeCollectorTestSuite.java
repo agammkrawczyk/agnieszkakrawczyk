09df2be3ae69a522acfa5e7b3857393a5e032642
agnieszka <agammkrawczyk@gmail.com>
2018-06-02 23:02:49 +0200

    correct testing method

--- kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
@@ -38,10 +38,11 @@ public class ShapeCollectorTestSuite {
         // Given
         ShapeCollector shapeCollector = new ShapeCollector();
         Square square = new Square( 5.0 );
+        Circle circle= new Circle( 4.0 )
 
         //When
          shapeCollector.addFigure( square );
-         shapeCollector.addFigure( square );
+         shapeCollector.addFigure( circle );
 
 
         //Then
