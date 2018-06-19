66a4b86e23d89e7bf6f265382ee831054a2ab1fb
agnieszka <agammkrawczyk@gmail.com>
2018-06-19 20:57:44 +0200

    method reference -functional calculator

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,13 +1,19 @@
 package com.kodilla.stream;
 import com.kodilla.stream.lambada.ExpressionExecutor;
+import com.kodilla.stream.reference.FunctionalCalculator;
 
 
 public class StreamMain {
     public static void main(String[]args){
        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-       expressionExecutor.executeExpresion( 10,5,(a, b) -> a+b );
        expressionExecutor.executeExpresion( 10,5,(a, b) -> a-b );
        expressionExecutor.executeExpresion( 10,5,(a, b) -> a*b );
        expressionExecutor.executeExpresion( 10,5,(a, b) -> a/b );
+
+       System.out.println("Calculating expressions with method references");
+       expressionExecutor.executeExpresion( 3,4,FunctionalCalculator::multiplyAByB);
+       expressionExecutor.executeExpresion( 3,4,FunctionalCalculator::addAToB );
+       expressionExecutor.executeExpresion( 3,4,FunctionalCalculator::subBFromA );
+       expressionExecutor.executeExpresion( 3,4,FunctionalCalculator::divideAByB );
     }
 }
