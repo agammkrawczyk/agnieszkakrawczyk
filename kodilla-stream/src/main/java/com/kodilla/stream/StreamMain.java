775ae9d8905564b47319b5c50ee3ba4279bcd776
agnieszka <agammkrawczyk@gmail.com>
2018-06-22 19:57:27 +0200

    Stream Collector joining- mod. main method

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -9,17 +9,16 @@ public class StreamMain {
 
     public static void main(String[] args) {
         BookDirectory theBookDirectory = new BookDirectory();
-
-        Map<String, Book> theResultMapOfBooks = theBookDirectory.getList().stream()
+        String theResultStringOfBooks = theBookDirectory.getList().stream()
                 .filter(book -> book.getYearOfPublication() > 2005)
-                .collect(Collectors.toMap(Book::getSignature, book -> book));
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n","<<",">>"));
 
-        System.out.println("# elements: " + theResultMapOfBooks.size());
-        theResultMapOfBooks.entrySet().stream()
-                .map(entry -> entry.getKey() + ": " + entry.getValue())
-                .forEach(System.out::println);
+        System.out.println(theResultStringOfBooks);
     }
 
-
-
 }
+
+
+
+
