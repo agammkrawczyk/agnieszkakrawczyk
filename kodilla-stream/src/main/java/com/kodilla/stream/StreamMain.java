fee029f2a764b18b34ace9216c8590c3c02882dc
agnieszka <agammkrawczyk@gmail.com>
2018-06-22 14:58:53 +0200

    stream book class

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,31 +1,14 @@
 package com.kodilla.stream;
-import com.kodilla.stream.person.People;
+import com.kodilla.stream.book.BookDirectory;
 
 public class StreamMain {
-    public static void main(String[] args) {
 
-       // People.getList().stream()
-             //.forEach(System.out::println);
-
-        //People.getList().stream()
-          //      .map( s -> s.toUpperCase() )
-           //     .forEach( System.out::println );
-
-
-
-       // People.getList().stream()
-        //        .map(String::toUpperCase)
-         //       .forEach(s -> System.out.println(s));
-
-        People.getList().stream()
-                .map(String::toUpperCase)
-                .filter(s -> s.length() > 11)
-                .map(s -> s.substring(0, s.indexOf(' ') + 2) + ".")
-                .filter(s -> s.substring(0, 1).equals("M"))
-                .forEach(System.out::println);
-
-
+        public static void main (String[]args){
+            BookDirectory theBookDirectory = new BookDirectory();
+            theBookDirectory.getList().stream()
+                    .filter( book -> book.getYearOfPublication() > 2005 )
+                    .forEach( System.out::println );
+        }
 
 
     }
-}
