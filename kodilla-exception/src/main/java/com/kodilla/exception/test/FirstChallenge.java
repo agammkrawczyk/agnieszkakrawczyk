24fccf15404171a9b293252751ff27396e86a4d1
agnieszka <agammkrawczyk@gmail.com>
2018-07-10 14:07:55 +0200

    First and second challenge- exception

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -2,7 +2,7 @@ package com.kodilla.exception.test;
 
 public class FirstChallenge {
     public double divide(double a, double b) throws ArithmeticException {
-        if(b == 0){
+        if (b == 0) {
             throw new ArithmeticException();
         }
         return a / b;
@@ -10,24 +10,23 @@ public class FirstChallenge {
 
     /**
      * This main can throw an ArithmeticException!!!
+     *
      * @param args
      */
     public static void main(String[] args) {
-    try {
-        FirstChallenge firstChallenge = new FirstChallenge();
+        try {
+            FirstChallenge firstChallenge = new FirstChallenge();
 
-        double result = firstChallenge.divide( 3, 1 );
+            double result = firstChallenge.divide( 3, 0 );
 
-        System.out.println( result );
-    }
-    catch(ArithmeticException e){
+            System.out.println( result );
+        } catch (ArithmeticException e) {
 
-    System.out.println( "Oh no smoething went wrong " +e);
+            System.out.println( "Oh no smoething went wrong " + e );
 
-    }
-    finally {
-   System.out.println(  " I am gona be here always no mather what");
-}
+        } finally {
+            System.out.println( " I am gona be here always no mather what" );
+        }
     }
 
 
