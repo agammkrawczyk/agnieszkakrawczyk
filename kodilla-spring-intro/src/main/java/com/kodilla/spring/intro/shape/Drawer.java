4e1354e27d1f9a148a63df9287a3f38370e3d7ce
agnieszka <agammkrawczyk@gmail.com>
2018-07-30 22:26:53 +0200

    add status

---
+++ kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Drawer.java
@@ -0,0 +1,12 @@
+package com.kodilla.spring.intro.shape;
+
+public class Drawer {
+    public void doDrawing() {
+        Circle circle = new Circle();
+        Triangle triangle = new Triangle();
+
+        circle.draw();
+        triangle.draw();
+    }
+
+}
---
+++ kodilla-spring-intro/src/test/java/com/kodilla/spring/intro/shape/DrawerTestSuite.java
@@ -0,0 +1,16 @@
+package com.kodilla.spring.intro.shape;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DrawerTestSuite {
+    @Test
+    public void testDoDrawing() {
+        //Given
+        Drawer drawer = new Drawer();
+        //When
+        drawer.doDrawing();
+        //Then
+        //do nothing
+    }
+}
