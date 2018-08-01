b58e5686a2b5f35850b9188f2b6bc8af10eaba1e
agnieszka <agammkrawczyk@gmail.com>
2018-08-01 22:32:53 +0200

    zle zle zle

--- kodilla-spring/src/test/java/com/kodilla/spring/SpringRunnerTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/SpringRunnerTestSuite.java
@@ -1,15 +1,15 @@
 package com.kodilla.spring;
 
+import com.kodilla.spring.shape.Circle;
+import com.kodilla.spring.shape.Triangle;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.kodilla.spring.shape.Circle;
 import com.kodilla.spring.shape.Shape;
-import com.kodilla.spring.shape.Triangle;
 
 import org.junit.Assert;
-import org.apache.catalina.core.ApplicationContext;
+
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
