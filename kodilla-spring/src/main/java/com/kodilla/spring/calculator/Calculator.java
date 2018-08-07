c18b18be2ff69b618db3a18623db9ae64c44c6f8
agnieszka <agammkrawczyk@gmail.com>
2018-08-07 22:50:56 +0200

    corect calculations

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -11,23 +11,28 @@ public class Calculator {
     public double  add(double a, double b){
         display.displayValue( a );
         display.displayValue( b );
+        display.displayValue( a+b );
         return a+b;
 
     }
     public double sub(double a, double b){
         display.displayValue( a );
         display.displayValue( b );
+        display.displayValue( a-b );
         return a-b;
     }
 
    public double mul(double a, double b) {
        display.displayValue( a );
        display.displayValue( b );
-        return    a*b;}
+       display.displayValue( a*b );
+        return    a*b;
+    }
 
    public double div(double a, double b){
        display.displayValue( a );
        display.displayValue( b );
+       display.displayValue( a/b );
         return a/b;
    }
 
