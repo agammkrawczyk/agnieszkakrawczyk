b7238000811c2ffc1f0a9dedb9042228347e0de8
agnieszka <agammkrawczyk@gmail.com>
2018-08-12 16:12:00 +0200

    Logger

--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -1,18 +1,17 @@
 package com.kodilla.patterns.singleton;
 
-import org.junit.Assert;
-import org.junit.Test;
+        import org.junit.Assert;
+        import org.junit.Test;
 
 public class LoggerTestSuite {
 
     @Test
     public void testGetLastLog() {
-   //Given
-    Logger logger= new Logger();
-    logger.log( "I was here" );
-    //When
-      String result=  logger.getLastLog();
-    //Then
+        //Given
+        Logger.getInstance().log( "I was here" );
+        //When
+        String result=Logger.getInstance().getLastLog();
+        //Then
         Assert.assertEquals( "I was here",result );
 
 
