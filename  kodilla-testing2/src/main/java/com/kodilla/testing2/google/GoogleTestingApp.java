8d28c99f0598a64a8a081ad6cc7682fd254d4464
agnieszka <agammkrawczyk@gmail.com>
2018-11-20 19:37:02 +0100

    selenium

---  kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
+++  kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -1,12 +1,19 @@
 package com.kodilla.testing2.google;
 
 import com.kodilla.testing.config.WebDriverConfig;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class GoogleTestingApp {
+    public static final String SEARCHFIELD = "q";
     public static void main(String[] args) {
-        WebDriver driver= WebDriverConfig.getDriver( WebDriverConfig.FIREFOX );
-        driver.get( "https://google.com" );
-    }
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://www.google.com");
 
+        WebElement searchField = driver.findElement(By.name(SEARCHFIELD));
+        searchField.sendKeys("Kodilla");
+
+
+    }
 }
