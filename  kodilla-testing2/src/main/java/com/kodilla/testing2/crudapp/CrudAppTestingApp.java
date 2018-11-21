d0e206e48cd1a2ffd688d2dc9157249223af9dd9
agnieszka <agammkrawczyk@gmail.com>
2018-11-21 22:07:15 +0100

    Xpath

---  kodilla-testing2/src/main/java/com/kodilla/testing2/crudapp/CrudAppTestingApp.java
+++  kodilla-testing2/src/main/java/com/kodilla/testing2/crudapp/CrudAppTestingApp.java
@@ -9,7 +9,10 @@ import org.openqa.selenium.support.ui.Select;
 public class CrudAppTestingApp {
     public static  final String XPATH_INPUT="//html/body/main/section/form/fieldset/input";
     public static final String XPATH_TEXTAREA = "//html/body/main/section/form/fieldset[2]/textarea";
-    public static final String XPATH_SELECT = "//select[1]";
+    public static final String WAIT_FOR = "//select[1]";
+    public static final String XPATH_SELECT = "//div[contains(@class, \"tasks-container\")]/form/div/fieldset/select[1]";
+
+
     public static void main(String[] args) {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
         driver.get("https://agammkrawczyk.github.io/");
@@ -19,9 +22,9 @@ public class CrudAppTestingApp {
         textareaField.sendKeys("New robotic content");
 
         while (!driver.findElement(By.xpath(XPATH_SELECT)).isDisplayed());
+
         WebElement selectCombo = driver.findElement(By.xpath(XPATH_SELECT));
         Select selectBoard = new Select(selectCombo);
         selectBoard.selectByIndex(1);
-
     }
 }
