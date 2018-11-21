fec74430e4ad123caafff9e1570d3e13af4bb09b
agnieszka <agammkrawczyk@gmail.com>
2018-11-21 21:29:59 +0100

    CrudAppTesting

---
+++  kodilla-testing2/src/main/java/com/kodilla/testing2/crudapp/CrudAppTestingApp.java
@@ -0,0 +1,20 @@
+package com.kodilla.testing2.crudapp;
+
+import com.kodilla.testing.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CrudAppTestingApp {
+    public static  final String XPATH_INPUT="//html/body/main/section/form/fieldset/input";
+    public static final String XPATH_TEXTAREA = "//html/body/main/section/form/fieldset[2]/textarea";
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://agammkrawczyk.github.io/");
+        WebElement searchField = driver.findElement(By.xpath(XPATH_INPUT));
+        searchField.sendKeys("New robotic task");
+        WebElement textareaField = driver.findElement(By.xpath(XPATH_TEXTAREA));
+        textareaField.sendKeys("New robotic content");
+
+    }
+}
